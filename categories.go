6e9aceee15017feacde357bb2b086d2edf577a48
Pavlo Sheshenia <sheshenia@outlook.com>
2022-07-02 23:59:42 +0300

    random image

--- categories.go
+++ categories.go
@@ -9,7 +9,12 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
+	"strings"
 	"time"
+)
+
+const (
+	DefaultImageHref = "https://telegram.org/img/t_logo.png"
 )
 
 type Media struct {
@@ -25,8 +30,7 @@ func NewMedia() (*Media, error) {
 	if err != nil {
 		return nil, fmt.Errorf("mediaCategories read dir: %w", err)
 	}
-	m := &Media{}
-	m.Categories = make(map[uint8]MediaCategory)
+	m := &Media{Categories: make(map[uint8]MediaCategory)}
 	for _, f := range files {
 		if f.IsDir() || filepath.Ext(f.Name()) != ".json" {
 			continue
@@ -69,3 +73,36 @@ type UIntSlice []uint8
 func (x UIntSlice) Len() int           { return len(x) }
 func (x UIntSlice) Less(i, j int) bool { return x[i] < x[j] }
 func (x UIntSlice) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+func (m *Media) extractCatIDs(catJoinedIDs uint8) []uint8 {
+	if catJoinedIDs == 0 || catJoinedIDs == m.IDsSum {
+		return m.IDs
+	}
+	cats := []uint8{}
+	for _, id := range m.IDs {
+		if id&catJoinedIDs > 0 {
+			cats = append(cats, id)
+		}
+	}
+	return cats
+}
+
+// randomImage 1 && 2 && 4 : can be 1, 2, 3, 4, 5, 6, 7  // but 0 - all
+func (m *Media) randomImage(catJoinedIDs uint8) string {
+	fromCats := m.extractCatIDs(catJoinedIDs)
+	n := rand.Intn(len(fromCats))
+
+	cat, ok := m.Categories[fromCats[n]]
+	if !ok {
+		log.Println("!ok MediaCategories[fromNames[catID]]", fromCats[n])
+		return DefaultImageHref
+	}
+	if len(cat.Files) == 0 {
+		return DefaultImageHref
+	}
+	fileName := cat.Files[rand.Intn(len(cat.Files))]
+	if strings.HasPrefix(fileName, "http") {
+		return fileName
+	}
+	return cat.Path + fileName
+}
